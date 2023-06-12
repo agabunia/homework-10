867cba180f04f1f409b38fc6766c14d534c593c7
Aleksandre Gabunia <97845038+agabunia@users.noreply.github.com>
2023-06-12 18:23:54 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './web-dev.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is my first React Projects.</h1>
+        <img src={logo} alt="logo" />
+        <p>To Be Continued</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
